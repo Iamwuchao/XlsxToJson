25a99fc33e4ba0388dcbe8ac65fef8274db51a7e
wuchao3 <wuchao3@xiaomi.com>
2016-06-17 16:36:44 +0800

    add java

---
+++ ExcelParser.java
@@ -0,0 +1,356 @@
+package utils.xlsx2json;
+
+import org.apache.poi.hssf.usermodel.HSSFDataFormat;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_BLANK;
+import static org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING;
+
+/**
+ * Created by Feliciano on 6/1/2016.
+ */
+public class ExcelParser {
+
+    public static String SIGN_HIDDEN_CELL_PREFIX = "$";
+
+    public static String SIGN_ITEM_SPLITTER = ",";
+
+    public static String SIGN_KEYVALUE_SPLITTER = ":";
+
+    public static String SIGN_TABLE_REFERENCE_SPLITTER = "@";
+
+    public static String SIGN_SHEETNAME_COLUMNNAME_SPLITTER = "#";
+
+
+    /**
+     * Parse the whole sheet of a workbook
+     * @param workbook
+     * @param configName
+     * @return
+     */
+    public static JSONArray parseSheet( Workbook workbook, String configName ) {
+        // Iterate through the rows.
+        JSONArray rows = new JSONArray();
+
+        // Get the Sheet by name.
+
+
+        ParsedSheet parsedSheet = new ParsedSheet(workbook, configName);
+        parsedSheet.parseSheet();
+
+        Sheet sheet = parsedSheet.getSheet();
+
+        // Parse each row.
+        for (Iterator<Row> rowsIT = sheet.rowIterator(); rowsIT.hasNext(); )
+        {
+            Row row = rowsIT.next();
+
+            if ( row.getRowNum() <= parsedSheet.nameRowIndex )
+                continue;
+
+            // Iterate through the cells.
+            JSONObject jsonRow = parseRow(row, parsedSheet);
+
+            rows.put( jsonRow );
+        }
+
+        return rows;
+    }
+
+    /**
+     * Find a row using the key and value in another sheet
+     * @param sheet The target sheet
+     * @param key One column's name of the row
+     * @param value The column's value
+     * @return Found row, or null if not found
+     */
+    public static Row findRowByColumn( ParsedSheet sheet, String key, String value) {
+        int index = sheet.indexOfKey(key);
+
+        if (index == -1)
+            throw new IllegalArgumentException("Couldn't find key " + key + " in the provided sheet.");
+
+        for (Iterator<Row> rowsIT = sheet.getSheet().rowIterator(); rowsIT.hasNext(); ) {
+            Row row = rowsIT.next();
+
+            Cell cell = row.getCell(index);
+
+            switch (sheet.getType(index)) {
+                case BASIC:
+                    if (cell == null)
+                        continue;
+
+                    if (cell.getCellType() == CELL_TYPE_BLANK)
+                        continue;
+
+                    String cellValue = getCellStringValue(cell);
+
+                    if (cellValue.equals(value))
+                        return row;
+
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Reference search doesn't support the type " + sheet.getType(index) + " of key " + key + ".");
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Parse a row of the sheet
+     * @param row The target row to parse
+     * @param parsedSheet Parsed sheet to provide name and type information
+     * @return A parsed JSONObject
+     */
+    public static JSONObject parseRow(Row row, ParsedSheet parsedSheet) {
+        JSONObject jsonRow = new JSONObject();
+
+        //Parse each cell
+        for ( int index = 0; index < parsedSheet.width;  index++)
+        {
+            Cell cell = row.getCell(index);
+
+            String key = parsedSheet.getKey( index );
+
+            if (key.startsWith(SIGN_HIDDEN_CELL_PREFIX))
+                continue;
+
+            ParsedCellType type = parsedSheet.getType( index );
+
+            // Null cell handler
+            switch (type) {
+                case BASIC:
+                    // Handle "Null" string
+                    if (cell != null && cell.getCellType() == CELL_TYPE_STRING) {
+                        if (cell.getStringCellValue().equalsIgnoreCase("null")) {
+                            jsonRow.put( key, JSONObject.NULL);
+                            continue;
+                        }
+                    }
+                case OBJECT:
+                    if (cell == null || cell.getCellType() == CELL_TYPE_BLANK) {
+                        jsonRow.put( key, JSONObject.NULL);
+                        continue;
+                    }
+                    break;
+                case REFERENCE:
+                    if (cell == null || cell.getCellType() == CELL_TYPE_BLANK) {
+                        jsonRow.put( key.substring(0, key.indexOf("@")), JSONObject.NULL);
+                        continue;
+                    }
+                    break;
+
+                case ARRAY_STRING:
+                case ARRAY_BOOLEAN:
+                case ARRAY_DOUBLE:
+                    if (cell == null || cell.getCellType() == CELL_TYPE_BLANK) {
+                        jsonRow.put( key, new ArrayList() );
+                        continue;
+                    }
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Unhandled empty cell of " + type + " type.");
+            }
+
+            ArrayList result;
+            JSONArray jsonArray;
+            JSONObject jsonObject;
+
+            switch (type) {
+                case BASIC:
+                    switch (cell.getCellType())
+                    {
+                        case Cell.CELL_TYPE_NUMERIC:
+                        	if (HSSFDateUtil.isCellDateFormatted(cell)) {
+	                        	 SimpleDateFormat sdf = null;  
+	                             if (cell.getCellStyle().getDataFormat() == HSSFDataFormat  
+	                                     .getBuiltinFormat("h:mm")) {  
+	                                 sdf = new SimpleDateFormat("HH:mm");  
+	                             } else {// 日期  
+	                                 sdf = new SimpleDateFormat("yyyy-MM-dd");  
+	                             }  
+	                             Date date = (Date) cell.getDateCellValue();  
+	                             String str = sdf.format(date);
+	                             jsonRow.put( key, str);
+                        	}
+                        	else
+                        		jsonRow.put( key, cell.getNumericCellValue() );
+                            break;
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            jsonRow.put( key, cell.getBooleanCellValue() );
+                            break;
+                        default:
+                            jsonRow.put( key, cell.getStringCellValue() );
+                            break;
+                    };
+                    break;
+
+                case ARRAY_STRING:
+                    result = ExcelParser.<ArrayList<String>>parseCellData(type, cell.getStringCellValue());
+                    jsonArray = new JSONArray(result);
+                    jsonRow.put( key, jsonArray );
+                    break;
+
+                case ARRAY_BOOLEAN:
+                    result = ExcelParser.<ArrayList<Boolean>>parseCellData(type, cell.getStringCellValue());
+                    jsonArray = new JSONArray(result);
+                    jsonRow.put( key, jsonArray );
+                    break;
+
+                case ARRAY_DOUBLE:
+                    result = ExcelParser.<ArrayList<Double>>parseCellData(type, cell.getStringCellValue());
+                    jsonArray = new JSONArray(result);
+                    jsonRow.put( key, jsonArray );
+                    break;
+
+                case OBJECT:
+                    jsonObject = ExcelParser.<JSONObject>parseCellData(type, cell.getStringCellValue());
+                    jsonRow.put( key, jsonObject );
+                    break;
+
+                case REFERENCE:
+                    // Split key to get real key, target sheet name and target column name
+                    // Key example: monster@monsterSheet#monsterId
+                    String[] keyAndTarget = key.split(SIGN_TABLE_REFERENCE_SPLITTER);
+                    key = keyAndTarget[0];
+
+                    // Split sheet name and column name
+                    String[] realTarget = keyAndTarget[1].split(SIGN_SHEETNAME_COLUMNNAME_SPLITTER);
+                    String targetSheetName = realTarget[0];
+                    String targetKey = realTarget[1];
+                    String targetValue = getCellStringValue(cell);
+
+                    Sheet targetSheet = parsedSheet.getSheet(targetSheetName);
+                    ParsedSheet parsedTargetSheet = new ParsedSheet(targetSheet.getWorkbook(), targetSheetName);
+                    parsedTargetSheet.parseSheet();
+
+                    Row targetRow = findRowByColumn(parsedTargetSheet, targetKey, targetValue);
+                    jsonObject = parseRow(targetRow, parsedTargetSheet);
+
+                    jsonRow.put( key, jsonObject);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Unsupported type " + type + " found.");
+            }
+
+        }
+        return jsonRow;
+    }
+
+    private static String getCellStringValue(Cell cell) {
+        switch (cell.getCellType()) {
+            case CELL_TYPE_BLANK:
+                break;
+
+            case Cell.CELL_TYPE_NUMERIC:
+                return cell.getNumericCellValue() + "";
+
+            case Cell.CELL_TYPE_BOOLEAN:
+                return cell.getBooleanCellValue() + "";
+
+            default:
+                return cell.getStringCellValue();
+        }
+
+        return null;
+    }
+
+    /**
+     * Parse a cell of the row
+     * @param type The data type
+     * @param cellValue The cell string to be parsed
+     * @param <W> The return data type
+     * @return Parsed data
+     * @throws NumberFormatException Numeric data parse failed
+     */
+    public static <W> W parseCellData(ParsedCellType type, String cellValue) throws NumberFormatException {
+        Object object = null;
+
+        String[] items = cellValue.split(SIGN_ITEM_SPLITTER);
+
+        switch (type) {
+            case ARRAY_STRING:
+                ArrayList<String> arrayString = new ArrayList<>();
+                for (String item : items) {
+                    item = item.trim();
+                    arrayString.add(item);
+                }
+                object = arrayString;
+                break;
+
+            case ARRAY_BOOLEAN:
+                ArrayList<Boolean> arrayBoolean = new ArrayList<>();
+                for (String item : items) {
+                    item = item.trim();
+                    arrayBoolean.add(Boolean.parseBoolean(item));
+                }
+                object = arrayBoolean;
+                break;
+
+            case ARRAY_DOUBLE:
+                ArrayList<Double> arrayDouble = new ArrayList<>();
+                for (String item : items) {
+                    item = item.trim();
+                    arrayDouble.add(Double.parseDouble(item));
+                }
+                object = arrayDouble;
+                break;
+
+            case OBJECT:
+                JSONObject obj = new JSONObject();
+
+                for (String item : items) {
+                    String temp = item.trim();
+
+                    String[] keyValue = item.split(SIGN_KEYVALUE_SPLITTER);
+                    String key = keyValue[0], value = keyValue[1];
+                    key = key.trim();
+                    value = value.trim();
+
+                    // Handle the null child
+                    if (value.equalsIgnoreCase("null")) {
+                        obj.put( key, JSONObject.NULL );
+                        continue;
+                    }
+
+                    if (value.startsWith("\"")) {
+                        obj.put( key, value.substring(1, value.length()-1));
+                        continue;
+                    }
+
+                    try {
+                        obj.put ( key, Double.parseDouble(value) );
+                    } catch (NumberFormatException e) {
+                        if (Boolean.parseBoolean(value)) {
+                            obj.put( key, true );
+                        } else if (value.equalsIgnoreCase("false")) {
+                            obj.put( key, false);
+                        } else {
+                            obj.put( key, value);
+                        }
+                    }
+                }
+
+                object = obj;
+                break;
+        }
+
+        return (W) object;
+    }
+
+}
---
+++ ExcelParserMain.java
@@ -0,0 +1,174 @@
+package utils.xlsx2json;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Feliciano on 6/1/2016.
+ */
+public class ExcelParserMain {
+
+    /**
+     * Read a workbook from the Excel file
+     * @param targetName File name
+     * @return The workbook
+     * @throws IOException
+     * @throws InvalidFormatException
+     */
+    public static Workbook getWorkbook(String targetName) throws IOException, InvalidFormatException {
+        File excelFile = new File(targetName + ".xlsx");
+        FileInputStream inp = new FileInputStream( excelFile );
+        
+        Workbook workbook = WorkbookFactory.create( inp );
+        return workbook;
+    }
+
+    /**
+     * Construct a json object using the sheet list from the workbook
+     * @param workbook
+     * @param sheetList Sheet names in the workbook
+     * @return Constructed json object
+     */
+    public static JSONObject constructJsonObject(Workbook workbook, String[] sheetList) {
+        // Start constructing JSON.
+        JSONObject json = new JSONObject();
+
+        // Create JSON
+        for (String sheetName : sheetList) {
+            JSONArray rows = ExcelParser.parseSheet(workbook, sheetName);
+            json.put(sheetName, rows);
+        }
+
+        return  json;
+    }
+
+    /**
+     * Construct a json array using the sheet list from the workbook
+     * @param workbook
+     * @param sheetList
+     * @return
+     */
+    public static JSONArray constructJsonArray(Workbook workbook, String[] sheetList) {
+        JSONArray json = new JSONArray();
+
+        // Create JSON
+        for (String sheetName : sheetList) {
+            JSONArray rows = ExcelParser.parseSheet(workbook, sheetName);
+            for (int i=0; i < rows.length(); i++) {
+                json.put(rows.get(i));
+            }
+        }
+
+        return json;
+    }
+
+    /**
+     * Save a string as a json file
+     * @param targetName
+     * @param jsonText
+     * @throws IOException
+     */
+    public static void saveStringToFile(String targetName, String jsonText) throws IOException {
+        // Write into file
+        Path path = Paths.get(targetName + ".json");
+        BufferedWriter writer = Files.newBufferedWriter( path );
+        writer.write(jsonText);
+        writer.close();
+    }
+
+    /**
+     * Parse the excel file and save as json
+     * @param targetName Excel file name without suffix
+     * @param sheetList Target sheet list
+     * @param showSheetName Whether show sheet name in result or not
+     */
+    public static String parseExcelFile(String targetName, String[] sheetList, boolean showSheetName) {
+
+
+        try {
+            Workbook workbook = getWorkbook(targetName);
+
+            String jsonText;
+
+            if (showSheetName) {
+                // Get the JSON text.
+                JSONObject json = constructJsonObject(workbook, sheetList);
+                jsonText = json.toString();
+            } else {
+                // Get the JSON text.
+                JSONArray json = constructJsonArray(workbook, sheetList);
+                jsonText = json.toString();
+            }
+            return jsonText;
+           // saveStringToFile(targetName, jsonText);
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * Validate two json file.  If it isn't it throws an {@link AssertionError}.
+     * @param expectedFileName Expected JSON file
+     * @param targetFileName File to compare
+     * @param strict Enables strict checking
+     * @throws JSONException
+     */
+    public static void validateJson(String expectedFileName, String targetFileName, boolean strict) {
+        try {
+            System.out.println("Checking " + targetFileName + " ... ");
+            Path path = Paths.get(targetFileName);
+            BufferedReader reader = Files.newBufferedReader(path);
+            String jsonText = reader.readLine();
+
+            path = Paths.get(expectedFileName);
+            reader = Files.newBufferedReader(path);
+            String expected = reader.readLine();
+
+            JSONAssert.assertEquals(expected, jsonText, false);
+
+            System.out.println("Passed.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String start(String[] args) {
+        if (args.length < 2)
+            throw new IllegalArgumentException("Expected at least 2 arguments representing Filename, Sheetnames(Divided by space, surrounded by \").");
+
+        if (args.length > 3)
+            throw new IllegalArgumentException("Expected at most 3 arguments representing Filename, Sheetnames(Divided by space, surrounded by \") and a boolean for show sheet names in result or not.");
+
+        String targetName = args[0];
+
+        if (targetName.equalsIgnoreCase("Validate")) {
+        	return "";
+        } else {
+            if (!targetName.endsWith("xlsx"))
+                throw new IllegalArgumentException("The first argument should be a excel(xlsx) file name.");
+            // Cut the .xlsx suffix
+            targetName = targetName.substring(0, targetName.length()-5);
+
+            // Split sheet names
+            String[] sheetList = args[1].split(" ");
+
+            // Detect show sheet name option
+            boolean showSheetName = (args.length == 3) ? Boolean.parseBoolean(args[2]) : false;
+
+            return parseExcelFile(targetName, sheetList, showSheetName);
+        }
+    }
+
+}
